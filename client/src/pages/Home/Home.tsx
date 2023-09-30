46b219d9d7603bfb1280f85cd7647f9b1fa4c268
Shalijar <ali0shajari@gmail.com>
2023-09-30 15:04:34 -0400

    added Recently Viewed Products Feature

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,12 +2,17 @@ import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { Page, ProductPreviewCard } from "../../components";
 import { ServiceAPI } from "../../infrastructure";
+import { useCookies } from "react-cookie";
 import "./Home.style.scss";
 import "./HomeDarkMode.scss";
+
 
 function Home() {
   const [products, setProducts] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(false);
+  const [recentlyViewedProducts, setRecentlyViewedProducts] = useCookies(["recentlyViewedProducts"]); 
+
+  
 
   useEffect(() => {
     const fetchData = async () => {
@@ -17,23 +22,30 @@ function Home() {
 
     fetchData();
 
-    // Check for dark mode preference
     const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
     setIsDarkMode(mediaQuery.matches);
 
-    // Listen for changes in the prefers-color-scheme and update the state accordingly
     const listener = (event) => {
       setIsDarkMode(event.matches);
     };
 
     mediaQuery.addListener(listener);
 
-    // Clean up the listener when the component unmounts
     return () => {
       mediaQuery.removeListener(listener);
     };
 
   }, []);
+
+  const handleProductClick = (product) => {
+    const currentRecentlyViewed = recentlyViewedProducts.recentlyViewedProducts || [];
+
+    const updatedRecentlyViewed = [product, ...currentRecentlyViewed];
+
+    const limitedRecentlyViewed = updatedRecentlyViewed.slice(0, 5);
+
+    setRecentlyViewedProducts("recentlyViewedProducts", limitedRecentlyViewed);
+  };
 
   return (
     <Page>
@@ -43,7 +55,11 @@ function Home() {
         <h2>Products:</h2>
         <div className="home-page__products">
           {products.map((product) => (
-            <Link to={`/products/${product.id}`} key={`${product.id}`}>
+            <Link
+              to={`/products/${product.id}`}
+              key={`${product.id}`}
+              onClick={() => handleProductClick(product)} // Handle product click
+            >
               <ProductPreviewCard
                 title={product.title}
                 description={product.description}
@@ -53,6 +69,23 @@ function Home() {
               />
             </Link>
           ))}
+        </div>
+        {/* Recently Viewed Products section */}
+        <div className="recently-viewed">
+          <h2>Recently Viewed Products</h2>
+          <div className="recently-viewed-products">
+            {recentlyViewedProducts.recentlyViewedProducts &&
+              recentlyViewedProducts.recentlyViewedProducts.map((product, index) => (
+                <Link to={`/products/${product.id}`} key={index}>
+                  <ProductPreviewCard
+                    title={product.title}
+                    description={product.description}
+                    price={product.price}
+                    imageUrl={product.imageUrl}
+                  />
+                </Link>
+              ))}
+          </div>
         </div>
       </div>
     </Page>
