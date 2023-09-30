4e30ee284c5878b3c37849f53fa46982c27175f9
Shalijar <ali0shajari@gmail.com>
2023-09-30 13:23:25 -0400

    added partial dark mode

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 import { Page, ProductPreviewCard } from "../../components";
 import { ServiceAPI } from "../../infrastructure";
 import "./Home.style.scss";
+import "./HomeDarkMode.scss";
 
 function Home() {
   const [products, setProducts] = useState([]);
+  const [isDarkMode, setIsDarkMode] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -14,13 +16,30 @@ function Home() {
     };
 
     fetchData();
+
+    // Check for dark mode preference
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    setIsDarkMode(mediaQuery.matches);
+
+    // Listen for changes in the prefers-color-scheme and update the state accordingly
+    const listener = (event) => {
+      setIsDarkMode(event.matches);
+    };
+
+    mediaQuery.addListener(listener);
+
+    // Clean up the listener when the component unmounts
+    return () => {
+      mediaQuery.removeListener(listener);
+    };
+
   }, []);
 
   return (
     <Page>
-      <div className="home-page">
+      <div className={`home-page ${isDarkMode ? 'adaptive' : ''}`}>
         <h1 className="home-page__title">Home</h1>
-        <p>Hello, wrold!</p>
+        <p>Hello, world!</p>
         <h2>Products:</h2>
         <div className="home-page__products">
           {products.map((product) => (
