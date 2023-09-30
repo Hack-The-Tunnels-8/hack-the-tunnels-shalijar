d17535d3bd2342547c44a7fa410cf892a2582239
Shalijar <ali0shajari@gmail.com>
2023-09-30 14:19:04 -0400

    added login form

--- client/src/pages/Login/Login.tsx
+++ client/src/pages/Login/Login.tsx
@@ -5,8 +5,8 @@ import { useAccountContext } from "../../context";
 import "./Login.style.scss";
 
 function Login() {
-  const [email, setEmail] = useState(""); // Add email state
-  const [password, setPassword] = useState(""); // Add password state
+  const [email, setEmail] = useState(""); 
+  const [password, setPassword] = useState(""); 
   const [message, setMessage] = useState(null);
   const { loggedIn, login } = useAccountContext();
   const navigate = useNavigate();
@@ -18,7 +18,7 @@ function Login() {
     } catch (error) {
       console.log(error);
       setMessage("Invalid credentials. Please try again."); 
-    } // Add the missing closing curly brace here
+    } 
   };
 
   useEffect(() => {
@@ -35,13 +35,13 @@ function Login() {
           type="email"
           placeholder="Email"
           value={email}
-          onChange={(e) => setEmail(e.target.value)} // Update email state
+          onChange={(e) => setEmail(e.target.value)} 
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
-          onChange={(e) => setPassword(e.target.value)} // Update password state
+          onChange={(e) => setPassword(e.target.value)} 
         />
         <button onClick={() => attemptLogin()}>Login</button>
         {message && <p className="error-message">{message}</p>}
